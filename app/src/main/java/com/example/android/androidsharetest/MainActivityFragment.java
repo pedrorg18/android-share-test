2d5fcff0d67f047534efd71ec9df6b2852cc1207
Pedro Roig <pedro.roig.gimenez@gmail.com>
2015-08-09 22:58:53 +0200

    No funciona bien del todo, hay que hacer que coja el texto dinamicamente.

--- app/src/main/java/com/example/android/androidsharetest/MainActivityFragment.java
+++ app/src/main/java/com/example/android/androidsharetest/MainActivityFragment.java
@@ -6,10 +6,12 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.widget.ShareActionProvider;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
@@ -21,9 +23,11 @@ import java.io.File;
  */
 public class MainActivityFragment extends Fragment {
 
+    private static final String LOG_TAG = MainActivityFragment.class.getSimpleName();
+
     EditText tvUserInput;
 
-    ShareActionProvider mShareActionProvider;
+    ShareActionProvider shareActionProvider;
 
 
     public MainActivityFragment() {
@@ -43,6 +47,12 @@ public class MainActivityFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
         tvUserInput = (EditText)rootView.findViewById(R.id.tvUserInput);
+//        tvUserInput.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+//            @Override
+//            public void onFocusChange(View v, boolean hasFocus) {
+//                setShareIntent(createShareIntent(getUserInput()));
+//            }
+//        });
 
         return rootView;
 
@@ -61,35 +71,58 @@ public class MainActivityFragment extends Fragment {
         // Get the menu item.
         MenuItem menuItem = menu.findItem(R.id.menu_item_share);
         // Get the provider and hold onto it to set/change the share intent.
-        mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
-        // Set history different from the default before getting the action
-        // view since a call to MenuItem.getActionView() calls
-        // onCreateActionView() which uses the backing file name. Omit this
-        // line if using the default share history file is desired.
-//        mShareActionProvider.setShareHistoryFileName("custom_share_history.xml");
-    }
+        shareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-
-        int id = item.getItemId();
-
-        if (id == R.id.menu_item_share) {
-            Intent shareIntent = new Intent(Intent.ACTION_SEND);
-            shareIntent.setType("text/plain");
-//            Uri uri = Uri.fromFile(new File(getFilesDir(), "foo.jpg"));
-            shareIntent.putExtra(Intent.EXTRA_SUBJECT, "Title Of The Post");
-            shareIntent.putExtra(Intent.EXTRA_TEXT, tvUserInput.getText());
-//            startActivity(Intent.createChooser(shareIntent, "Share link!"));
-            doShare(shareIntent);
-            return true;
+        for (int i = 0; i < menu.size(); i++) {
+            MenuItem item = menu.getItem(i);
+            if (item.getItemId() == R.id.menu_item_share) {
+                View itemChooser = item.getActionView();
+                if (itemChooser != null) {
+                    itemChooser.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            setShareIntent(createShareIntent(getUserInput()));
+                        }
+                    });
+                }
+            }
         }
 
-        return true;
+        //Ponemos un listener para cuando se pulse el boton compartir
+        shareActionProvider.setOnShareTargetSelectedListener(new ShareActionProvider.OnShareTargetSelectedListener() {
+            @Override
+            public boolean onShareTargetSelected(ShareActionProvider actionProvider, Intent intent) {
+                //Actualizamos el Intent con el texto del usuario
+                setShareIntent(createShareIntent(getUserInput()));
+                return false;
+            }
+        });
+
+        setShareIntent(createShareIntent(getUserInput()));
+//        if(shareActionProvider!=null){
+//            shareActionProvider.setShareIntent(createShareIntent("Holaaaaaa"));
+//        }else{
+//            Log.d(LOG_TAG, "Share Action Provider is null?");
+//        }
+
     }
 
-    public void doShare(Intent shareIntent) {
-        // When you want to share set the share intent.
-        mShareActionProvider.setShareIntent(shareIntent);
+    // Call to update the share intent
+    private void setShareIntent(Intent shareIntent) {
+        if (shareActionProvider != null) {
+            shareActionProvider.setShareIntent(shareIntent);
+        }else{
+            Log.d(LOG_TAG, "Share Action Provider is null?");
+        }
+    }
+
+    private Intent createShareIntent(String textToShare){
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        //Para que al terminar de compartir vuelva a nuestra app y no la que gestiona el intent
+        shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_TEXT,
+                textToShare);
+        return shareIntent;
     }
 }
